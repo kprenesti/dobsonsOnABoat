0f30ddcc2a9d4447d7a0eff9e1f49128451fa79c
kprenesti <krista.prenesti@gmail.com>
2016-02-12 21:56:31 -0500

    Fixed tab panel functionality.

--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-  //select the tab
-  $('li.tab').on('click touchstart', function(){
-    $('.active').removeClass('active');
-    $(this).addClass('active');
-    var panelId = "#" + $(this).attr('rel');
-    var panelHTML = $(panelId).html();
-    $('.tab-content').html(panelHTML);
-  });
 
+    $('.allTabs .tab').on('click touchstart', function(){
+      var panelToShow = $(this).attr('rel');
+      $('.tab.active').removeClass('active');
+      $(this).addClass('active');
+      $('.panel.active').removeClass('active');      $('#'+panelToShow).addClass('active');
+    }); //end click
 
 }); //end ready
